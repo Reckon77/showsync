7ea2d4acb130d8cbb485aeb71278aa3e8a6ced8c
Reckon Mazumdar <reckonmazumdar12@gmail.com>
2024-04-18 12:56:05 +0530

    added : swagger

--- src/main/java/com/ticket/booking/showsync/controller/Home.java
+++ src/main/java/com/ticket/booking/showsync/controller/Home.java
@@ -1,25 +1,25 @@
 package com.ticket.booking.showsync.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Home {
     @PreAuthorize("hasAuthority('USER')")
-    @RequestMapping("/user")
+    @GetMapping("/user")
     public String test() {
         return "User message";
     }
 
     @PreAuthorize("hasAuthority('ADMIN')")
-    @RequestMapping("/admin")
+    @GetMapping("/admin")
     public String test1() {
         return "Admin message";
     }
 
     @PreAuthorize("hasAuthority('ADMIN') or hasAuthority('USER')")
-    @RequestMapping("/super")
+    @GetMapping("/super")
     public String test2() {
         return "Super Admin message";
     }
--- src/main/java/com/ticket/booking/showsync/controller/UserController.java
+++ src/main/java/com/ticket/booking/showsync/controller/UserController.java
@@ -4,6 +4,7 @@ import com.ticket.booking.showsync.dto.JwtRequest;
 import com.ticket.booking.showsync.dto.JwtResponse;
 import com.ticket.booking.showsync.dto.UserDTO;
 import com.ticket.booking.showsync.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -18,11 +19,12 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Operation(summary = "Register a new user or admin")
     @PostMapping("/register")
     public ResponseEntity<UserDTO> registerUser(@Valid @RequestBody UserDTO user) {
         return userService.registerUser(user);
     }
-
+    @Operation(summary = "User login")
     @PostMapping("/login")
     public ResponseEntity<JwtResponse> login(@RequestBody JwtRequest jwtRequest){
         return userService.login(jwtRequest);
